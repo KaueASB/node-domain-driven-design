493cf0c00ff3a5755a1875713b4f1edddeaf7de6
Kaue Alves <kaue_alves00@yahoo.com>
2024-02-21 20:46:43 -0300

    test: Use case - delete question

---
+++ src/domains/forum/application/use-cases/delete-question.spec.ts
@@ -0,0 +1,50 @@
+import { DeleteQuestionsUseCase } from './delete-question'
+import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
+import { makeQuestion } from 'test/factories/make-question'
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository
+let sut: DeleteQuestionsUseCase
+
+describe('Delete question', () => {
+  beforeEach(() => {
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository()
+    sut = new DeleteQuestionsUseCase(inMemoryQuestionsRepository)
+  })
+
+  it('should be able to delete a question by id', async () => {
+    const newQuestion = makeQuestion(
+      { authorId: new UniqueEntityId('author-1') },
+      new UniqueEntityId('question-1'),
+    )
+
+    await inMemoryQuestionsRepository.create(newQuestion)
+
+    expect(inMemoryQuestionsRepository.items).toHaveLength(1)
+
+    await sut.execute({
+      authorId: 'author-1',
+      questionId: 'question-1',
+    })
+
+    expect(inMemoryQuestionsRepository.items).toHaveLength(0)
+  })
+
+  it('should not be able to delete a question from another author', async () => {
+    const newQuestion = makeQuestion(
+      { authorId: new UniqueEntityId('author-1') },
+      new UniqueEntityId('question-1'),
+    )
+
+    await inMemoryQuestionsRepository.create(newQuestion)
+
+    expect(inMemoryQuestionsRepository.items).toHaveLength(1)
+
+    await expect(() =>
+      sut.execute({
+        authorId: 'another-author',
+        questionId: 'question-1',
+      }),
+    ).rejects.toThrowError('You can only delete your own questions.')
+  })
+})
