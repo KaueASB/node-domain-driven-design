c8a14ceda788bd669d3670682b08755b4f9259f1
Kaue Alves <kaue_alves00@yahoo.com>
2024-02-21 16:11:50 -0300

    feat: create new  propertys

--- src/domains/entities/answer.ts
+++ src/domains/entities/answer.ts
@@ -5,6 +5,8 @@ export interface AnswerProps {
   authorId: UniqueEntityId
   questionId: UniqueEntityId
   content: string
+  createdAt: Date
+  updatedAt?: Date
 }
 
 export class Answer extends Entity<AnswerProps> {
--- src/domains/entities/question.ts
+++ src/domains/entities/question.ts
@@ -4,9 +4,12 @@ import { UniqueEntityId } from "../../core/entities/unique-entity-id"
 
 export interface QuestionProps {
   authorId: UniqueEntityId
+  bestAnswerId?: UniqueEntityId
   title: string
   content: string
   slug: Slug
+  createdAt: Date
+  updatedAt?: Date
 }
 
 export class Question extends Entity<QuestionProps> {
