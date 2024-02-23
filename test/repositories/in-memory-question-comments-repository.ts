e06621c18d8d9c05209026f5714dbab2a0155624
Kaue Alves <kaue_alves00@yahoo.com>
2024-02-23 12:22:56 -0300

    feat: Use case - fetch question comments

--- test/repositories/in-memory-question-comments-repository.ts
+++ test/repositories/in-memory-question-comments-repository.ts
@@ -1,3 +1,4 @@
+import { PaginationParams } from '@/core/repositories/pagination-params'
 import { QuestionCommentsRepository } from '@/domains/forum/application/repositories/question-comments-repository'
 import { QuestionComment } from '@/domains/forum/enterprise/entities/question-comment'
 
@@ -16,6 +17,17 @@ export class InMemoryQuestionCommentsRepository
     }
 
     return questionComment
+  }
+
+  async findManyByQuestionId(
+    questionId: string,
+    { page }: PaginationParams,
+  ): Promise<QuestionComment[]> {
+    const questionComments = this.items
+      .filter((item) => item.questionId.toString() === questionId)
+      .slice((page - 1) * 20, page * 20)
+
+    return questionComments
   }
 
   async create(questionComment: QuestionComment) {
