d1eb37523bcb679a86b9c832e1c82099e113b6e2
Kaue Alves <kaue_alves00@yahoo.com>
2024-02-23 10:58:24 -0300

    test: Use case - comment on answer

---
+++ src/domains/forum/application/use-cases/comment-on-answer.spec.ts
@@ -0,0 +1,36 @@
+import { CommentOnAnswerUseCase } from './comment-on-answer'
+import { InMemoryAnswerCommentsRepository } from 'test/repositories/in-memory-answer-comments-repository'
+import { InMemoryAnswersRepository } from 'test/repositories/in-memory-answers-repository'
+import { makeAnswer } from 'test/factories/make-answer'
+
+let inMemoryAnswersRepository: InMemoryAnswersRepository
+let inMemoryAnswerCommentsRepository: InMemoryAnswerCommentsRepository
+let sut: CommentOnAnswerUseCase
+
+describe('Comment on answer', () => {
+  beforeEach(() => {
+    inMemoryAnswersRepository = new InMemoryAnswersRepository()
+    inMemoryAnswerCommentsRepository = new InMemoryAnswerCommentsRepository()
+
+    sut = new CommentOnAnswerUseCase(
+      inMemoryAnswersRepository,
+      inMemoryAnswerCommentsRepository,
+    )
+  })
+
+  it('should be able to comment on answer', async () => {
+    const newAnswer = makeAnswer()
+
+    await inMemoryAnswersRepository.create(newAnswer)
+
+    await sut.execute({
+      authorId: newAnswer.authorId.toString(),
+      answerId: newAnswer.id.toString(),
+      content: 'Comment on answer test',
+    })
+
+    expect(inMemoryAnswerCommentsRepository.items[0].content).toBe(
+      'Comment on answer test',
+    )
+  })
+})
