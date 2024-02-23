3ea6b13ec85284769c911ad22ac934489a4bc2fe
Kaue Alves <kaue_alves00@yahoo.com>
2024-02-23 12:34:07 -0300

    feat: Use case - fetch answer comments

--- test/repositories/in-memory-answer-comments-repository.ts
+++ test/repositories/in-memory-answer-comments-repository.ts
@@ -1,3 +1,4 @@
+import { PaginationParams } from '@/core/repositories/pagination-params'
 import { AnswerCommentsRepository } from '@/domains/forum/application/repositories/answer-comments-repository'
 import { AnswerComment } from '@/domains/forum/enterprise/entities/answer-comment'
 
@@ -16,6 +17,17 @@ export class InMemoryAnswerCommentsRepository
     }
 
     return answerComment
+  }
+
+  async findManyByAnswerId(
+    answerId: string,
+    { page }: PaginationParams,
+  ): Promise<AnswerComment[]> {
+    const answerComments = this.items
+      .filter((item) => item.answerId.toString() === answerId)
+      .slice((page - 1) * 20, page * 20)
+
+    return answerComments
   }
 
   async create(answerComment: AnswerComment) {
