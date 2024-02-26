77edad2f68a9bb943a95b7f8e5e18b5e90c9d5f4
Kaue Alves <kaue_alves00@yahoo.com>
2024-02-26 19:55:29 -0300

    feat: Domain Events - Listening to the Answer Created domain event

--- src/core/events/domain-events.ts
+++ src/core/events/domain-events.ts
@@ -2,7 +2,7 @@ import { AggregateRoot } from '../entities/aggregate-root'
 import { UniqueEntityId } from '../entities/unique-entity-id'
 import { DomainEvent } from './domain-event'
 
-type DomainEventCallback = (event: unknown) => void
+type DomainEventCallback = (event: any) => void // eslint-disable-line
 
 export class DomainEvents {
   private static handlersMap: Record<string, DomainEventCallback[]> = {}
---
+++ src/domains/notification/application/subscribers/on-answer-created.ts
@@ -0,0 +1,35 @@
+import { DomainEvents } from '@/core/events/domain-events'
+import { EventHandler } from '@/core/events/event-handler'
+import { QuestionsRepository } from '@/domains/forum/application/repositories/questions-repository'
+import { AnswerCreatedEvent } from '@/domains/forum/enterprise/events/answer-created-event'
+import { SendNotificationUseCase } from '../use-cases/send-notification'
+
+export class OnAnswerCreated implements EventHandler {
+  constructor(
+    private questionsRepository: QuestionsRepository,
+    private sendNotificationUseCase: SendNotificationUseCase,
+  ) {
+    this.setupSubscriptions()
+  }
+
+  setupSubscriptions() {
+    DomainEvents.register(
+      this.sendNewAnswerNotification.bind(this),
+      AnswerCreatedEvent.name,
+    )
+  }
+
+  private async sendNewAnswerNotification({ answer }: AnswerCreatedEvent) {
+    const question = await this.questionsRepository.findById(
+      answer.questionId.toString(),
+    )
+
+    if (question) {
+      this.sendNotificationUseCase.execute({
+        recipientId: question.authorId.toString(),
+        title: `Nova resposta em ${question.title.substring(0, 40).concat('...')}`,
+        content: answer.excerpt,
+      })
+    }
+  }
+}
